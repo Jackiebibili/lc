54a3519df1108629c047dba9f250338871d9f08b
Jack Li <jacky984479215@outlook.com>
2023-05-04 23:34:35 -0500

    add state compression problem solution

---
+++ dp/luogu/statecompress/NQueens.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+   /**
+    * @brief
+    *
+    * @param row: current row number
+    * @param y: binary representation of where cannot (1) place queen for vertical attack
+    * @param rtl: binary representation of where cannot (1) place queen for right to left diagonal (/)
+    * @param ltr: binary representation of where cannot (1) place queen for left to right diagonal (\)
+    * @param n: total number of rows
+    * @return int: the number of ways to place n queens in nxn grid
+    */
+   int dfs(int row, int y, int rtl, int ltr, int n) {
+      int ans = 0;
+      int queenplmtbin = ~(y | rtl | ltr) & (1 << n) - 1;
+
+      while (queenplmtbin) {
+         // find rightmost 1 bit
+         int rowqueenplmtbin = queenplmtbin & -queenplmtbin;
+         if (row == n) {
+            ans++;
+         } else {
+            ans += dfs(row + 1, y | rowqueenplmtbin, (rtl | rowqueenplmtbin) << 1, (ltr | rowqueenplmtbin) >> 1, n);
+         }
+         // change the rightmost 1 bit to 0 bit
+         queenplmtbin ^= rowqueenplmtbin;
+      }
+
+      return ans;
+   }
+   int totalNQueens(int n) {
+      return dfs(1, 0, 0, 0, n);
+   }
+};
